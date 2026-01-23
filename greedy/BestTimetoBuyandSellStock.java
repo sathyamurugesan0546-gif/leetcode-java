028166550d8b12a37318a045bcacc76c240db75b
Sathya Murugesan <sathyamurugesan0546@gmail.com>
2026-01-23 22:38:21 +0530

    Improved edge case handling and renamed variables for clarity

--- greedy/BestTimetoBuyandSellStock.java
+++ greedy/BestTimetoBuyandSellStock.java
@@ -1,22 +1,22 @@
 // Pattern: Greedy + Prefix Minimum
-// Core move: Track lowest buy so far + best profit
-// Lower price  → update buy
+// Core move: Track lowest minPrice so far + best profit
+// Lower price  → update minPrice
 // Higher price → try selling, update maxProfit
 // Reason it works: Best sell depends on min price before it
-// Buy always happens before sell (left → right scan)
+// minPrice always happens before sell (left → right scan)
 // Time: O(n)
 // Space: O(1)
  
  
 class BestTimetoBuyandSellStock{
     public int maxProfit(int[] prices) {
-        int buy=prices[0];
+        int minPrice=prices[0];
         int maxProfit=0;
         for(int i=1;i<prices.length;i++){
-            if(buy>prices[i]){
-                buy=prices[i];
+            if(minPrice>prices[i]){
+                minPrice=prices[i];
             }
-            int profit=prices[i]-buy;
+            int profit=prices[i]-minPrice;
             if(profit>maxProfit){
                 maxProfit=profit;
             }
